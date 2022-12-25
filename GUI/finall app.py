e122e40948d696b308e70a7a3af2393e025b2168
princeo741 <113790710+princeo741@users.noreply.github.com>
2022-12-25 15:50:16 +0530

    Added ASL button

--- GUI/finall app.py
+++ GUI/finall app.py
@@ -16,13 +16,13 @@ def run_file_2():
 def run_file_3():
     exec(open('Basic/Simple-Hand-Tracker.py').read())
 
+def run_file_4():
+    exec(open('Version 1.1/app.py').read())
 
 # Create the buttons
 button1 = tk.Button(text="Actions", command=run_file_1)
 button1.configure(height=5, width=20)
 button1.pack(pady=10, padx=10)
-
-
 
 button2 = tk.Button(text="Rock Paper Scissor", command=run_file_2)
 button2.configure(height=5, width=20)
@@ -32,10 +32,14 @@ button3 = tk.Button(text="Simple-Hand-Tracker", command=run_file_3)
 button3.configure(height=5, width=20)
 button3.pack(pady=10, padx=10)
 
+button4 = tk.Button(text="ASL", command=run_file_4)
+button4.configure(height=5, width=20)
+button4.pack(pady=10, padx=10)
+
 # Pack the buttons into the window
 button1.pack()
 button2.pack()
 button3.pack()
-
+button4.pack()
 # Run the main loop
 window.mainloop()
