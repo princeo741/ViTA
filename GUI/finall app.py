66b09d9d92a962725660bba14c3bf0a2c8e72dc3
princeo741 <113790710+princeo741@users.noreply.github.com>
2022-12-25 18:16:32 +0530

    GUI Refined

--- GUI/finall app.py
+++ GUI/finall app.py
@@ -1,10 +1,28 @@
 import tkinter as tk
+# Import the PhotoImage class from the tkinter module
+from tkinter import PhotoImage
+
 import os
 
 
 # Create the main window
 window = tk.Tk()
 window.title("ViTA")
+window.geometry("450x450")
+window.config(bg="white")
+
+# Load the image file
+image = PhotoImage(file="GUI/LOGO.png")
+# Resize the image while preserving the aspect ratio
+image = image.subsample(2, 2)
+# Create an image label widget
+label = tk.Label(image=image)
+# Pack the image label widget to the left of the parent widget
+label.pack(side="left")
+
+# Create a button widget
+button = tk.Button(window, image=image, bg="white")
+#button.pack()
 
 # Define a function for each button that runs the corresponding Python file
 def run_file_1():
@@ -20,19 +38,20 @@ def run_file_4():
     exec(open('Version 1.1/app.py').read())
 
 # Create the buttons
-button1 = tk.Button(text="Actions", command=run_file_1)
+
+button1 = tk.Button(text="Actions", bg="beige", command=run_file_1)
 button1.configure(height=5, width=20)
 button1.pack(pady=10, padx=10)
 
-button2 = tk.Button(text="Rock Paper Scissor", command=run_file_2)
+button2 = tk.Button(text="Rock Paper Scissor", bg="beige", command=run_file_2)
 button2.configure(height=5, width=20)
 button2.pack(pady=10, padx=10)
 
-button3 = tk.Button(text="Simple-Hand-Tracker", command=run_file_3)
+button3 = tk.Button(text="Simple-Hand-Tracker", bg="beige", command=run_file_3)
 button3.configure(height=5, width=20)
 button3.pack(pady=10, padx=10)
 
-button4 = tk.Button(text="ASL", command=run_file_4)
+button4 = tk.Button(text="ASL", bg="beige", command=run_file_4)
 button4.configure(height=5, width=20)
 button4.pack(pady=10, padx=10)
 
