be6da6f2d48ee804bf4808ac33771d0e6cb6c9b4
princeo741 <113790710+princeo741@users.noreply.github.com>
2022-12-26 20:44:21 +0530

    Added time feature

---
+++ Basic/time.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import time
+
+# Create the main window
+window = tk.Tk()
+window.title("Time")
+window.geometry("200x50")
+window.config(bg="white")
+
+# Create a label widget to display the time
+time_label = tk.Label(window, text="", font=("Helvetica", 18, "bold"))
+time_label.pack()
+
+# Update the time label every second
+def update_time():
+    current_time = time.strftime("%I:%M:%S %p")  # Get the current time in hh:mm:ss AM/PM format
+    time_label.configure(text=current_time)
+    window.after(1000, update_time)  # Run this function again after 1000 milliseconds (1 second)
+
+update_time()  # Initialize the time label
+window.mainloop()
